163703d4d90a1dfacf4fffa37882f0ca01e5547e
Kai_EV <kasicer1@gmail.com>
2023-02-25 19:37:00 +0100

    Initial Start project

---
+++ app/src/main/java/com/example/proyectob_pmdm_t2_kaiscervasquez/retrofitdata/APIRestService.java
@@ -0,0 +1,6 @@
+package com.example.proyectob_pmdm_t2_kaiscervasquez.retrofitdata;
+
+public interface APIRestService {
+
+    //TODO 2: Crear una interfaz que contenga los métodos que se van a utilizar para hacer las peticiones a la API
+}
---
+++ app/src/main/java/com/example/proyectob_pmdm_t2_kaiscervasquez/retrofitdata/RetrofitClient.java
@@ -0,0 +1,5 @@
+package com.example.proyectob_pmdm_t2_kaiscervasquez.retrofitdata;
+
+public class RetrofitClient {
+    //TODO 1: Crear una instancia de la clase Retrofit
+}
